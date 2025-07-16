9de7b4abc88b78c768ea222ae4c56209a3e626f8
erik <erik.riedel@tum.de>
2025-07-16 18:55:51 +0200

    prune graph claude

--- extract_single_seg_features.py
+++ extract_single_seg_features.py
@@ -6,23 +6,20 @@ from vvl.utils.GraphInfo import GraphInfo
 # Configuration
 volume_path = r"D:\data\joshua\Atlas\vessel_preds\vp\preds038\R_20241106145816_ATLAS001_532nm_uperarm1_RSOM50_corr.nii.gz"  # Set this to your file path
 
-resolution = [3, 12, 12]
-filter_length = 250
+resolution = [0.003, 0.012, 0.012]
+filter_length = 0.250
 prune_length = 0.0
-vol_spacing = np.array([0.003, 0.012, 0.012])
-large_vessel_radius = 14
+large_vessel_radius = 14.4
+
 
 graph_info = GraphInfo(
     volume_path,
     resolution=resolution,
-    vol_spacing=vol_spacing,
     filter_length=filter_length,
     prune_length=prune_length,
 )
 
 graph_info.extract_graph()
-
-graph_info.extract_features()
 
 graph_info.large_vessel_radius = large_vessel_radius
 
--- vvl/utils/GraphInfo.py
+++ vvl/utils/GraphInfo.py
@@ -18,7 +18,7 @@ class GraphInfo:
     ):
         self.volume_path = volume_path
         self.name = os.path.basename(volume_path).replace(".nii.gz", "")
-        self.unfiltered_vol = load_volume(volume_path)
+        self.unfiltered_vol, _ = load_volume(volume_path)
         self.resolution = resolution
         self.filter_length = filter_length
         self.prune_length = prune_length
@@ -51,6 +51,50 @@ class GraphInfo:
                 raise
 
         return extract_radius(self.nx_graph)
+
+    def vessel_depth_is_lower(self, x, y, z):
+        pass
+
+    def prune_graph_upper_lower(self):
+        # Create two new graphs of the same type as the original
+        lower_graph = self.nx_graph.__class__()
+        upper_graph = self.nx_graph.__class__()
+
+        # Copy graph attributes
+        lower_graph.graph.update(self.nx_graph.graph)
+        upper_graph.graph.update(self.nx_graph.graph)
+
+        # Sets to track which nodes should be included in each graph
+        lower_nodes = set()
+        upper_nodes = set()
+
+        # Process edges and classify them
+        for n1, n2, data in self.nx_graph.edges(data=True):
+            coord_list = data["coords_list"]
+
+            any_lower = any(vessel_depth_is_lower(x, y, z) for x, y, z in coord_list)
+            any_upper = any(not vessel_depth_is_lower(x, y, z) for x, y, z in coord_list)
+
+            if any_lower:
+                lower_graph.add_edge(n1, n2, **data)
+                lower_nodes.add(n1)
+                lower_nodes.add(n2)
+            if any_upper:
+                upper_graph.add_edge(n1, n2, **data)
+                upper_nodes.add(n1)
+                upper_nodes.add(n2)
+
+        # Add node attributes for nodes that are in the graphs
+        for node in lower_nodes:
+            if node in self.nx_graph.nodes:
+                lower_graph.add_node(node, **self.nx_graph.nodes[node])
+
+        for node in upper_nodes:
+            if node in self.nx_graph.nodes:
+                upper_graph.add_node(node, **self.nx_graph.nodes[node])
+
+        self.lower_graph = lower_graph
+        self.upper_graph = upper_graph
 
     def extract_features(self):
         self.features.update(
